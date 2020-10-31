78afc53384359d3ff810efd9cb05f7fd09bbe6f1
Mohammad Asif <69226688+masifamu@users.noreply.github.com>
2020-10-31 11:05:32 +0530

    Working perfectly( STM32CubeMX and KeilV5)

---
+++ Src/bldc.c
@@ -0,0 +1,374 @@
+#include "adc.h"
+#include "dma.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+#include "string.h"
+#include "stdio.h"
+
+#include "bldc.h"
+
+#define TIM1CH1(x) TIM1->CCR1=x
+#define TIM1CH2(x) TIM1->CCR2=x
+#define TIM1CH3(x) TIM1->CCR3=x
+
+#define CH1 1
+#define CH2 2
+#define CH3 3
+
+#ifdef UART_HALL_DEBUG
+char printDataString1[50] = "buffer here\r\n";//{'\0',};
+#endif
+
+uint8_t BLDC_MotorSpin = 0,toUpdate=0;
+uint8_t BLDC_STATE[6] = {0,0,0,0,0,0};
+uint16_t PWMWIDTH=0;
+
+#ifndef BLDC_PWMCOMPLEMENTARYMODE
+uint8_t BLDC_STATE_PREV[6] = {0,0,0,0,0,0};
+#endif
+/*
+// BLDC motor steps tables
+static const uint8_t BLDC_BRIDGE_STATE_FORWARD[8][6] =   // Motor steps
+{
+//	UH,UL		VH,VL	WH,WL
+   { 0,0	,	0,0	,	0,0 },  // 0 //000
+   { 0,1	,	0,0	,	1,0 },
+   { 1,0	,	0,1	,	0,0 },
+   { 0,0	,	0,1	,	1,0 },
+   { 0,0	,	1,0	,	0,1 },
+   { 0,1	,	1,0	,	0,0 },
+   { 1,0	,	0,0	,	0,1 },
+   { 0,0	,	0,0	,	0,0 },  // 0 //111
+};
+*/
+// BLDC motor steps tables
+static const uint8_t BLDC_BRIDGE_STATE_FORWARD[8][6] =   // Motor steps
+{
+//	UH,UL		VH,VL	WH,WL
+   { 0,0	,	0,0	,	0,0 },  // 0 //000
+   { 0,1	,	0,0	,	1,0 },
+   { 0,0	,	1,0	,	0,1 },
+   { 0,1	,	1,0	,	0,0 },
+   { 1,0	,	0,1	,	0,0 },
+   { 0,0	,	0,1	,	1,0 },
+   { 1,0	,	0,0	,	0,1 },
+   { 0,0	,	0,0	,	0,0 },  // 0 //111
+};
+
+static const uint8_t BLDC_BRIDGE_STATE_BACKWARD[8][6] =   // Motor steps
+{
+//	UH,UL		VH,VL	WH,WL
+   { 0,0	,	0,0	,	0,0 },  //  //000
+   { 1,0	,	0,0	,	0,1 },
+   { 0,1	,	1,0	,	0,0 },
+   { 0,0	,	1,0	,	0,1 },
+   { 0,0	,	0,1	,	1,0 },
+   { 1,0	,	0,1	,	0,0 },
+   { 0,1	,	0,0	,	1,0 },
+   { 0,0	,	0,0	,	0,0 },  //  //111
+};
+
+void BLDC_Init(void) {
+	//selectOCMChannel(TIM_CHANNEL_1);
+	//selectOCMChannel(TIM_CHANNEL_2);
+	//selectOCMChannel(TIM_CHANNEL_3);
+	BLDC_MotorStart();
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_pin) {
+  //interrupt on pins are being reset from the EXTI interrupt handler   
+  // Commutation
+	HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_3);
+	BLDC_MotorCommutation(BLDC_HallSensorsGetPosition());
+}
+
+
+uint8_t BLDC_HallSensorsGetPosition(void) {
+	uint8_t temp=(uint8_t)((GPIOB->IDR) & (GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7))>>5;
+	
+	#ifdef UART_HALL_DEBUG
+  snprintf(printDataString1,50, "hall pattern = %d\n\r", (uint16_t)temp);
+  HAL_UART_Transmit(&huart1, (uint8_t*)printDataString1, strlen(printDataString1), HAL_MAX_DELAY);
+	#endif
+	
+	return temp;
+}
+
+uint8_t BLDC_MotorGetSpin(void) {
+	return BLDC_MotorSpin;
+}
+
+void BLDC_MotorSetSpin(uint8_t spin) {
+	BLDC_MotorSpin = spin;
+}
+
+void BLDC_MotorStop(void)
+{
+	BLDC_SetPWM(16);
+}
+
+void BLDC_MotorStart(void)
+{
+		BLDC_SetPWM(0);
+	
+		HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+		HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2); 
+		HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3); 
+
+		//TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Disable);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
+		//TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Disable);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+		//TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Disable);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
+
+		BLDC_MotorSpin = 0;
+		#ifndef BLDC_PWMCOMPLEMENTARYMODE
+			memset(BLDC_STATE_PREV, 0, sizeof(BLDC_STATE_PREV));
+		#endif
+}
+//void BLDC_TurnLowerSwitchesOff(void){
+
+//}
+
+#ifdef BLDC_PWMTOPBOTTOMKEYS
+void BLDC_MotorCommutation(uint16_t hallpos)
+{
+	if (BLDC_MotorSpin == BLDC_CW) {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_FORWARD[hallpos], sizeof(BLDC_STATE));
+	}
+	else {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_BACKWARD[hallpos], sizeof(BLDC_STATE));
+	}
+
+	// Disable if need
+	if (!BLDC_STATE[UH]) TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Disable);
+	if (!BLDC_STATE[UL]) TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Disable);
+	if (!BLDC_STATE[VH]) TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Disable);
+	if (!BLDC_STATE[VL]) TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Disable);
+	if (!BLDC_STATE[WH]) TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Disable);
+	if (!BLDC_STATE[WL]) TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Disable);
+
+	// Enable if need. If previous state is Enabled then not enable again. Else output do flip-flop.
+	if (BLDC_STATE[UH] & !BLDC_STATE[UL] & !BLDC_STATE_PREV[UH]) TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+	if (BLDC_STATE[UL] & !BLDC_STATE[UH] & !BLDC_STATE_PREV[UL]) TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+	if (BLDC_STATE[VH] & !BLDC_STATE[VL] & !BLDC_STATE_PREV[VH]) TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Enable);
+	if (BLDC_STATE[VL] & !BLDC_STATE[VH] & !BLDC_STATE_PREV[VL]) TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Enable);
+	if (BLDC_STATE[WH] & !BLDC_STATE[WL] & !BLDC_STATE_PREV[WH]) TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+	if (BLDC_STATE[WL] & !BLDC_STATE[WH] & !BLDC_STATE_PREV[WL]) TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+
+	memcpy(BLDC_STATE_PREV, BLDC_STATE, sizeof(BLDC_STATE));
+}
+#endif
+
+#ifdef BLDC_PWMTOPKEYS
+void BLDC_MotorCommutation(uint16_t hallpos)
+{
+	if (BLDC_MotorSpin == BLDC_CW) {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_FORWARD[hallpos], sizeof(BLDC_STATE));
+	}
+	else {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_BACKWARD[hallpos], sizeof(BLDC_STATE));
+	}
+
+	// Disable if need
+	if (!BLDC_STATE[UH]) /*HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);*/TIM1CH1(0);
+	if (!BLDC_STATE[UL]) HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	if (!BLDC_STATE[VH]) /*HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);*/TIM1CH2(0);
+	if (!BLDC_STATE[VL]) HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	if (!BLDC_STATE[WH]) /*HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_3);*/TIM1CH3(0);
+	if (!BLDC_STATE[WL]) HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+
+	// Enable if need. If previous state is Enabled then not enable again. Else output do flip-flop.
+	if (BLDC_STATE[UH] & !BLDC_STATE[UL] & !BLDC_STATE_PREV[UH]) {
+		//selectOCMChannel(TIM_CHANNEL_1);
+		//TIM_CCxChannelCmd(TIM1,TIM_CHANNEL_1,TIM_CCx_ENABLE);
+		//HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+		//TIM1CH1(PWMWIDTH); 
+		toUpdate=CH1;
+	}
+	if (BLDC_STATE[UL] & !BLDC_STATE[UH] & !BLDC_STATE_PREV[UL]) {
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	}
+	if (BLDC_STATE[VH] & !BLDC_STATE[VL] & !BLDC_STATE_PREV[VH]) {
+		//selectOCMChannel(TIM_CHANNEL_2);
+		//TIM_CCxChannelCmd(TIM1,TIM_CHANNEL_2,TIM_CCx_ENABLE);
+		//HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+		//TIM1CH2(PWMWIDTH); 
+		toUpdate=CH2;
+	}
+	if (BLDC_STATE[VL] & !BLDC_STATE[VH] & !BLDC_STATE_PREV[VL]) {
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	}
+	if (BLDC_STATE[WH] & !BLDC_STATE[WL] & !BLDC_STATE_PREV[WH]) {
+		//selectOCMChannel(TIM_CHANNEL_3);
+		//TIM_CCxChannelCmd(TIM1,TIM_CHANNEL_3,TIM_CCx_ENABLE);
+		//HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+		//TIM1CH3(PWMWIDTH); 
+		toUpdate=CH3;
+	}
+	if (BLDC_STATE[WL] & !BLDC_STATE[WH] & !BLDC_STATE_PREV[WL]) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+	}
+	memcpy(BLDC_STATE_PREV, BLDC_STATE, sizeof(BLDC_STATE));
+}
+#endif
+
+#ifdef BLDC_PWMBOTTOMKEYS
+void BLDC_MotorCommutation(uint16_t hallpos)
+{
+	if (BLDC_MotorSpin == BLDC_CW) {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_FORWARD[hallpos], sizeof(BLDC_STATE));
+	}
+	else {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_BACKWARD[hallpos], sizeof(BLDC_STATE));
+	}
+
+	// Disable if need
+	if (!BLDC_STATE[UH]) TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Disable);
+	if (!BLDC_STATE[UL]) TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Disable);
+	if (!BLDC_STATE[VH]) TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Disable);
+	if (!BLDC_STATE[VL]) TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Disable);
+	if (!BLDC_STATE[WH]) TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Disable);
+	if (!BLDC_STATE[WL]) TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Disable);
+
+	// Enable if need. If previous state is Enabled then not enable again. Else output do flip-flop.
+	if (BLDC_STATE[UH] & !BLDC_STATE[UL] & !BLDC_STATE_PREV[UH]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_ForcedAction_Active);
+		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+	}
+	if (BLDC_STATE[UL] & !BLDC_STATE[UH] & !BLDC_STATE_PREV[UL]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_OCMode_PWM1);
+		TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+	}
+	if (BLDC_STATE[VH] & !BLDC_STATE[VL] & !BLDC_STATE_PREV[VH]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_ForcedAction_Active);
+		TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Enable);
+	}
+	if (BLDC_STATE[VL] & !BLDC_STATE[VH] & !BLDC_STATE_PREV[VL]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_OCMode_PWM1);
+		TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Enable);
+	}
+	if (BLDC_STATE[WH] & !BLDC_STATE[WL] & !BLDC_STATE_PREV[WH]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_ForcedAction_Active);
+		TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+	}
+	if (BLDC_STATE[WL] & !BLDC_STATE[WH] & !BLDC_STATE_PREV[WL]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_OCMode_PWM1);
+		TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+	}
+
+	memcpy(BLDC_STATE_PREV, BLDC_STATE, sizeof(BLDC_STATE));
+}
+#endif
+
+
+
+#ifdef BLDC_PWMCOMPLEMENTARYMODE
+void BLDC_MotorCommutation(uint16_t hallpos) {
+	if (BLDC_MotorSpin == BLDC_CW) {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_FORWARD[hallpos], sizeof(BLDC_STATE));
+	}
+	else {
+		memcpy(BLDC_STATE, BLDC_BRIDGE_STATE_BACKWARD[hallpos], sizeof(BLDC_STATE));
+	}
+
+	// PWM at low side FET of bridge U
+	// active freewheeling at high side FET of bridge U
+	// if low side FET is in PWM off mode then the hide side FET
+	// is ON for active freewheeling. This mode needs correct definition
+	// of dead time otherwise we have shoot-through problems
+
+	if (BLDC_STATE[UH]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_OCMode_PWM1);
+		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+		TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+	} else {
+		// Low side FET: OFF
+		TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Disable);
+		if (BLDC_STATE[UL]){
+			// High side FET: ON
+			TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_ForcedAction_Active);
+			TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+		} else {
+			// High side FET: OFF
+			TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Disable);
+		}
+	}
+
+	if (BLDC_STATE[VH]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_OCMode_PWM1);
+		TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Enable);
+		TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Enable);
+	} else {
+		// Low side FET: OFF
+		TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Disable);
+		if (BLDC_STATE[VL]){
+			// High side FET: ON
+			TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_ForcedAction_Active);
+			TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Enable);
+		} else {
+			// High side FET: OFF
+			TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Disable);
+		}
+	}
+
+	if (BLDC_STATE[WH]) {
+		TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_OCMode_PWM1);
+		TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+		TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+	} else {
+		// Low side FET: OFF
+		TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Disable);
+		if (BLDC_STATE[WL]){
+			// High side FET: ON
+			TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_ForcedAction_Active);
+			TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+		} else {
+			// High side FET: OFF
+			TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Disable);
+		}
+	}
+
+}
+#endif
+
+uint16_t BLDC_ADCToPWM(uint16_t ADC_VALUE) {
+	uint32_t tmp;
+
+	if (ADC_VALUE < BLDC_ADC_STOP) {
+		return 0;
+	}
+
+	if (ADC_VALUE > BLDC_ADC_MAX) {
+		return BLDC_CHOPPER_PERIOD+1;
+	}
+
+	tmp = (uint32_t)(ADC_VALUE-BLDC_ADC_STOP) * (uint32_t)BLDC_CHOPPER_PERIOD / (uint32_t)(BLDC_ADC_MAX - BLDC_ADC_START);
+
+	//to maintain the lower PWM width
+	return (uint16_t) tmp;
+
+}
+
+void BLDC_SetPWM(uint16_t PWM)
+{
+	PWMWIDTH=PWM;
+	if(toUpdate == CH1){
+		TIM1CH1(PWMWIDTH);
+	}else if(toUpdate == CH2){
+		TIM1CH2(PWMWIDTH);
+	}else if(toUpdate == CH3){
+		TIM1CH3(PWMWIDTH);
+	}
+}
+
+void selectOCMChannel(uint32_t channel){
+	TIM_OC_InitTypeDef sConfigOC;
+	
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, channel);
+}
---
+++ Src/main.c
@@ -0,0 +1,230 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "dma.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "bldc.h"
+#include "stdio.h"
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+#ifdef UART_COMM_DEBUG
+char printDataString[50] = "buffer here\r\n";//{'\0',};
+#endif
+
+uint16_t ADCBuffer[6]={0,};
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	uint16_t pwmWidth=0;
+	uint16_t throtle=0;
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC_Init();
+  MX_TIM1_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_ADCEx_Calibration_Start(&hadc);
+	HAL_ADC_Start_DMA(&hadc,(uint32_t*)&ADCBuffer,6);
+	
+	BLDC_Init();
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		throtle=ADCBuffer[0];
+		
+		#ifdef UART_COMM_DEBUG
+		snprintf(printDataString,50, "pwm = %d, throtle = %d\n\r", (uint16_t)pwmWidth,throtle);
+		HAL_UART_Transmit(&huart1, (uint8_t*)printDataString, strlen(printDataString), HAL_MAX_DELAY);
+		#endif
+		
+    if (throtle > BLDC_ADC_START) {
+			if (BLDC_MotorGetSpin() == BLDC_STOP) {
+				// Check Reverse pin
+				if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) != 0) {
+					// Forward
+					BLDC_MotorSetSpin(BLDC_CW);
+				}else{
+					// Backward
+					BLDC_MotorSetSpin(BLDC_CCW);
+				}
+				//pwmWidth=BLDC_ADCToPWM(throtle);
+				//BLDC_SetPWM(pwmWidth);
+				BLDC_MotorCommutation(BLDC_HallSensorsGetPosition());
+			}
+    	pwmWidth=BLDC_ADCToPWM(throtle);
+			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,GPIO_PIN_RESET);
+			BLDC_SetPWM(pwmWidth);
+    }else{
+			HAL_GPIO_WritePin(GPIOB,GPIO_PIN_4,GPIO_PIN_SET);
+			if (BLDC_MotorGetSpin() != BLDC_STOP) {
+				//meaning motor is still running
+				if (throtle < BLDC_ADC_STOP) {
+				BLDC_MotorStop();
+				}
+			}
+    }
+   }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
